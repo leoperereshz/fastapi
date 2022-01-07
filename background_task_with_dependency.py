9f5d52af4e50630ad2f677610f677ceaadee5818
Leonardo Peres <38218066+leoperereshz@users.noreply.github.com>
2022-01-07 14:56:26 -0300

    background tasks

---
+++ background_task_with_dependency.py
@@ -0,0 +1,28 @@
+# In this example, the messages will be written to the log.txt file after the response is sent.
+# If there was a query in the request, it will be written to the log in a background task.
+# And then another background task generated at the path operation function will write a message using the email path parameter.
+
+from fastapi import BackgroundTasks, Depends, FastAPI
+
+app = FastAPI()
+
+
+def write_log(message: str):
+    with open("log.txt", mode="a") as log:
+        log.write(message)
+
+
+def get_query(background_tasks: BackgroundTasks, q: str | None = None):
+    if q:
+        message = f"found query: {q}\n"
+        background_tasks.add_task(write_log, message)
+    return q
+
+
+@app.post("/send-notification/{email}")
+async def send_notification(
+    email: str, background_tasks: BackgroundTasks, q: str = Depends(get_query)
+):
+    message = f"message to {email}\n"
+    background_tasks.add_task(write_log, message)
+    return {"message": "Message sent"}
