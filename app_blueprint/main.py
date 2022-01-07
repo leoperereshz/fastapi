d55793d7fb05d6fadfb5b6d960a9b507d2aa5513
Leonardo Peres <38218066+leoperereshz@users.noreply.github.com>
2022-01-07 15:08:51 -0300

    metadatas no openApi

--- app_blueprint/main.py
+++ app_blueprint/main.py
@@ -4,8 +4,52 @@ from .dependencies import get_query_token, get_token_header
 from .internal import admin
 from .routers import items, users
 
-app = FastAPI(dependencies=[Depends(get_query_token)])
+description = """
+ChimichangApp API helps you do awesome stuff. 🚀
 
+## Items
+
+You can **read items**.
+
+## Users
+
+You will be able to:
+
+* **Create users** (_not implemented_).
+* **Read users** (_not implemented_).
+"""
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
+
+app = FastAPI(
+    title="ChimichangApp",
+    description=description,
+    openapi_tags=tags_metadata,
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Deadpoolio the Amazing",
+        "url": "http://x-force.example.com/contact/",
+        "email": "dp@x-force.example.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+    dependencies=[Depends(get_query_token)]
+)
 
 app.include_router(users.router)
 app.include_router(items.router)
